6dd74a20798055074acb3183584daa07e3efd8a7
paulodutra <paulodutra.info@gmail.com>
2023-03-01 10:41:48 -0300

    feat: add test of usecase register on mailing list

---
+++ src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
@@ -0,0 +1,15 @@
+import { UserData } from './user-data'
+
+describe('Register user on mailing list use case', () => {
+    test('should add user with complete data to mailing list', async () => {
+       const users: UserData[] = []
+       console.log(users)
+    //    const repo: UserRepository = new InMemoryUserRepository(users)
+    //    const useCase: RegisterUserOnMailingList = new RegisterUserOnMailingList(repo)
+    //    const name = 'user name'
+    //    const email = 'user@email.com'
+    //    const response =  await useCase.registerUserOnMailingList({ name, email })
+    //    const user = repo.findUserByEmail(`${email}`)
+    //    expect((await user).name).toBe('user name')
+    })
+})
