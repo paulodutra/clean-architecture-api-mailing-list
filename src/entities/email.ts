c3e4daee39e9056a1d35df6299596a0cdde4bfd6
paulodutra <paulodutra.info@gmail.com>
2023-03-07 17:00:30 -0300

    refactor: rename email to value on email value object

--- src/entities/email.ts
+++ src/entities/email.ts
@@ -2,10 +2,10 @@ import { Either, right, left } from '../shared/either'
 import { InvalidEmailError } from './errors/invalid-email-error'
 
 export class Email {
-    private readonly email: string
+    private readonly value: string
 
-    private constructor (email: string) {
-        this.email = email
+    private constructor (value: string) {
+        this.value = value
     }
     static create (email: string): Either<InvalidEmailError, Email> {
         if (Email.validate(email)) {
