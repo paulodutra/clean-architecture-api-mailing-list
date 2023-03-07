caf5cf65b5884ad478c184623eda14a4b4744cbc
paulodutra <paulodutra.info@gmail.com>
2023-03-07 13:27:03 -0300

    feat: validate emails with empty domain

--- src/entities/email.ts
+++ src/entities/email.ts
@@ -10,7 +10,7 @@ export class Email {
        if (localPart.length === 0 || localPart.length > 64) {
         return false                                               
        }
-       if (domain.length > 255) {
+       if (domain.length === 0 || domain.length > 255) {
         return false 
        }
        return true
