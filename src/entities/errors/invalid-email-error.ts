2d282267d54e7a00c19fdb6eec97a99bc9b4b646
paulodutra <paulodutra.info@gmail.com>
2023-03-09 16:49:36 -0300

    feat: improve error classes

--- src/entities/errors/invalid-email-error.ts
+++ src/entities/errors/invalid-email-error.ts
@@ -1,3 +1,8 @@
 export class InvalidEmailError extends Error {
+    
+    public readonly name = 'InvalidEmailError'
 
+    constructor (email: string) {
+        super('Invalid email: ' + email + '.')
+    }
 }
--- src/entities/errors/invalid-name-error.ts
+++ src/entities/errors/invalid-name-error.ts
@@ -1,3 +1,8 @@
 export class InvalidNameError extends Error {
     
+    public readonly name = 'InvalidNameError'
+
+    constructor (name: string) {
+        super('Invalid name: ' + name + '.')
+    }
 }
