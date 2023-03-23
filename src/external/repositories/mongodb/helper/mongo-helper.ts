704294ebcaf265323bb0f168f99162b3ecf87d6a
paulodutra <paulodutra.info@gmail.com>
2023-03-23 18:20:56 -0300

    feat: add user and verify existence using mongodb

--- src/external/repositories/mongodb/helper/mongo-helper.ts
+++ src/external/repositories/mongodb/helper/mongo-helper.ts
@@ -1,17 +1,17 @@
-import { MongoClient, Collection } from 'mongodb'
+import { MongoClient, MongoClientOptions, Collection } from 'mongodb'
 
 export const MongoHelper = {
    client: null as MongoClient,
    async connect (uri: string): Promise<void> {
-     this.client = await MongoClient.connect(uri)   
+    this.client = await MongoClient.connect(uri)   
    },
    async disconnect (): Promise<void> {
-     this.client.close()
+    this.client.close()
    },
    getCollection (name: string): Collection {
     return this.client.db().collection(name)
    },
-   clearCollection (name: string): void {
+   async clearCollection (name: string): Promise<void> {
        this.client.db().collection(name).deleteMany({})
    }
 }
