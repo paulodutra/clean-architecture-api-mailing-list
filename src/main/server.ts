c0ac01c9d90c59204eff5156e433129a0ba2f9bf
paulodutra <paulodutra.info@gmail.com>
2023-03-29 15:37:00 -0300

    chore: alter string of connection database'

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,14 +1,12 @@
 import 'module-alias/register'
 import { MongoHelper } from '@/external/repositories/mongodb/helper'
 
-
-MongoHelper.connect(process.env.MONGO_URL)
-    .then(async () => {
-        const app = (await import('./config/app')).default
-        app.listen(5000, () => {
-            console.log('Server running at http://localhost:5000')
-        })
-        
+MongoHelper.connect('mongodb://localhost:27017/clean-arch')
+  .then(async () => {
+    const app = (await import('./config/app')).default
+    app.listen(process.env.PORT || 5000, () => {
+      console.log('Server running...')
     })
-    .catch(console.error)
+  })
+  .catch(console.error)
 
