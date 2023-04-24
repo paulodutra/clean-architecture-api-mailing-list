7208edf51b1d271227c790df7d2f85aa0ea60592
paulodutra <paulodutra.info@gmail.com>
2023-04-24 18:58:40 -0300

    test: verify invalid data

--- test/usecases/register-and-send-email/register-and-send-email.spec.ts
+++ test/usecases/register-and-send-email/register-and-send-email.spec.ts
@@ -59,4 +59,30 @@ describe('Register and send email to user', () => {
         expect(mailServiceMock.timesSendWasCalled).toEqual(1)
         expect(response.value.name).toEqual('user name')
     })
+
+    test('should not add user with invalid email to mailing list', async () => {
+        const users: UserData[] = []
+        const repo: UserRepository = new InMemoryUserRepository(users)
+        const registerUseCase: RegisterUserOnMailingList = new RegisterUserOnMailingList(repo)
+        const name = 'user name'
+        const invalidEmail = 'useremail.com'
+        const mailServiceMock = new MailServiceMock()
+        const sendEmailUseCase = new SendEmail(mailOptions, mailServiceMock) 
+        const registerAndSendEmailUseCase: RegisterAndSendEmail = new RegisterAndSendEmail(registerUseCase, sendEmailUseCase)
+        const response = (await registerAndSendEmailUseCase.perform({ name, email: invalidEmail })).value as Error
+        expect(response.name).toEqual('InvalidEmailError')
+    })
+
+    test('should not add user with invalid name to mailing list', async () => {
+        const users: UserData[] = []
+        const repo: UserRepository = new InMemoryUserRepository(users)
+        const registerUseCase: RegisterUserOnMailingList = new RegisterUserOnMailingList(repo)
+        const invalidName = ''
+        const email = 'user@email.com'
+        const mailServiceMock = new MailServiceMock()
+        const sendEmailUseCase = new SendEmail(mailOptions, mailServiceMock) 
+        const registerAndSendEmailUseCase: RegisterAndSendEmail = new RegisterAndSendEmail(registerUseCase, sendEmailUseCase)
+        const response = (await registerAndSendEmailUseCase.perform({ name: invalidName, email })).value as Error
+        expect(response.name).toEqual('InvalidNameError')
+    })
 })
