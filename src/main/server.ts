2b4fea2f41b5b334eaced7ba7bca79d1ac5cee34
paulodutra <paulodutra.info@gmail.com>
2023-04-25 18:34:21 -0300

    refactor: use enviroment variable

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,7 +1,7 @@
 import 'module-alias/register'
 import { MongoHelper } from '@/external/repositories/mongodb/helper'
 
-MongoHelper.connect('mongodb://localhost:27017/clean-arch')
+MongoHelper.connect(process.env.MONGO_URL)
   .then(async () => {
     const app = (await import('./config/app')).default
     app.listen(process.env.PORT || 5000, () => {
